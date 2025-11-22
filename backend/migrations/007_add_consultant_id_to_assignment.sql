73d5eedd6f006b2c3ceaf2efc67e6af511a72e37
Amanda Krohn <amanda.krohn@svt.se>
2025-11-22 11:55:13 +0100

    fix table name

--- backend/migrations/007_add_consultant_id_to_assignment.sql
+++ backend/migrations/007_add_consultant_id_to_assignment.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE assignment ADD consultant_id INTEGER REFERENCES consultants(id) ON DELETE SET NULL;
+ALTER TABLE assignments ADD consultant_id INTEGER REFERENCES consultants(id) ON DELETE SET NULL;
 
 -- +goose Down
-ALTER TABLE assignment DROP COLUMN consultant_id;
+ALTER TABLE assignments DROP COLUMN consultant_id;
